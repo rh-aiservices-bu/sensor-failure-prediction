155bb3414c23b7561ed796408b3bb276d4585019
Cory Latschkowski <koree@redhat.com>
2022-07-27 21:12:37 -0600

    fix: issues with gunicorn and ERR_INCOMPLETE_CHUNKED_ENCODING

---
+++ flaskplotly2/config.py
@@ -0,0 +1,9 @@
+import os
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+
+bind = "0.0.0.0:8080"
+
+forwarded_allow_ips = '*'
+secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
