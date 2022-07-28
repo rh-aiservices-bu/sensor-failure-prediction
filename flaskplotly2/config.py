92520f2e17dff65b8400f3b18cbb62f3e02fbaef
Cory Latschkowski <koree@redhat.com>
2022-07-27 23:31:14 -0600

    update: threads, gunicorn

--- flaskplotly2/config.py
+++ flaskplotly2/config.py
@@ -1,7 +1,7 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
-threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
 bind = "0.0.0.0:8080"
 timeout = 90
