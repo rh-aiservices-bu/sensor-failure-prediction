ae937fe2aa9eea437821de62e22305aa7e919900
Cory Latschkowski <koree@redhat.com>
2022-07-27 22:52:45 -0600

    fix: gunicorn

--- flaskplotly2/config.py
+++ flaskplotly2/config.py
@@ -4,6 +4,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 
 bind = "0.0.0.0:8080"
+timeout = 90
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
