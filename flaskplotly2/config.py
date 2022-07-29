19acd7ae22bb1881a972b43b0513f064a5fb8b89
Cory Latschkowski <koree@redhat.com>
2022-07-28 21:32:15 -0700

    remove: timeout for gunicorn

--- flaskplotly2/config.py
+++ flaskplotly2/config.py
@@ -4,7 +4,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
 bind = "0.0.0.0:8080"
-timeout = 90
+#timeout = 90
 accesslog = "-"
 
 forwarded_allow_ips = '*'
