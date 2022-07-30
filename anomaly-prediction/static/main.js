5531bce71ad226658c22003dae5f3977b2b742fc
guiderae <guiderae@yahoo.com>
2022-07-30 09:04:33 -0700

    New js to handle the asynchronous loading of display data

---
+++ anomaly-prediction/static/main.js
@@ -0,0 +1,22 @@
+let displayDataDiv = document.getElementById("displayDataDiv");
+window.onload = async function(){
+    console.log("window.onload");
+    let url = '/initData'
+    postInitData(url)
+        .then(jsonData =>  displayJson(jsonData))
+        .catch(error => console.error(error))
+}
+
+async function postInitData(url){
+    return fetch(url,{
+        method: 'POST',
+        contentType: "application/json"
+    })
+        .then((response) => response.json());
+
+}
+function displayJson(jsonData){
+   // Parse json, put values into html page elements
+    displayDataDiv.innerHTML = jsonData.data;
+
+}
