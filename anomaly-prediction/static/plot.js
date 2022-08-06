86b021150faa0a18a08a8e8c0dd9a1df029862db
guiderae <guiderae@yahoo.com>
2022-08-06 15:23:28 -0700

    Changed marker colors for data points traces.  Changed the markers for the alarm to red vertical lines.  Removed unused traces.  Added graph title, changed top margin.  Changed code to unpack json.  Adjusted code in Plotly.extendTraces() to accommodate 3 traces.

--- anomaly-prediction/static/plot.js
+++ anomaly-prediction/static/plot.js
@@ -3,88 +3,58 @@ let data = [{ // 0 Trace for Data points for sensor
         x: [],
         y: [],
         mode: 'markers',
-        marker: {color: 'black', size: 3},
+        marker: {color: 'gray', size: 3},
         xaxis:{type: 'date'}
     },
     { // 1 Trace for Data points for sensor
         x: [],
         y: [],
         mode: 'markers',
-        marker: {color: 'black', size: 3},
+        marker: {color: 'gray', size: 3},
         xaxis:{type: 'date'}
     },
-    { // 2 Trace for Data points for sensor
+    { // 2 Trace for alarm
         x: [],
         y: [],
-        mode: 'markers',
-        marker: {color: 'black', size: 3},
+        yaxis: 'y2',
+        mode: 'lines',
+        marker: {color: 'red', size: 1},
         xaxis:{type: 'date'}
-    },
-    { // 3 Trace for Data points for sensor
-        x: [],
-        y: [],
-        mode: 'markers',
-        marker: {color: 'black', size: 3},
-        xaxis:{type: 'date'}
-    },
-
-    {   // Trace for alarm
-           x:[],
-           y:[],
-           yaxis: 'y2',
-
-           // plotcolor: ['green'],
-           mode: 'lines',
-           marker: {color: 'red'},
-           line: {width: 2},
-           opacity: 0.6
     }];
+
+
 // This array of empty traces is used whenever we need to restart a plot after it has been stopped.
 // Since the array is empty, the restarted plot will start with no data.
 let initData = [{ // 0 Trace for Data points for sensor
         x: [],
         y: [],
         mode: 'markers',
-        marker: {color: 'black', size: 3},
+        marker: {color: 'gray', size: 3},
         xaxis:{type: 'date'}
     },
     { // 1 Trace for Data points for sensor
         x: [],
         y: [],
         mode: 'markers',
-        marker: {color: 'black', size: 3},
+        marker: {color: 'gray', size: 3},
         xaxis:{type: 'date'}
     },
-    { // 2 Trace for Data points for sensor
+    { // 2 Trace for alarm
         x: [],
         y: [],
-        mode: 'markers',
-        marker: {color: 'black', size: 3},
+        yaxis: 'y2',
+        mode: 'lines',
+        marker: {color: 'red', size: 1},
         xaxis:{type: 'date'}
-    },
-    { // 3 Trace for Data points for sensor
-        x: [],
-        y: [],
-        mode: 'markers',
-        marker: {color: 'black', size: 3},
-        xaxis:{type: 'date'}
-    },
-
-    {   // 4 Trace for alarm
-           x:[],
-           y:[],
-           yaxis: 'y2',
-
-           // plotcolor: ['green'],
-           mode: 'lines',
-           marker: {color: 'red'},
-           line: {width: 2},
-           opacity: 0.6
     }];
     let layout = {
-        margin: {t:100},
+        title: {text: 'Failure Prediction',
+                font: {size: 20},
+                xanchor: 'center',
+                yanchor: 'top'},
+        margin: {t:50},
         xaxis: {type: 'date'},
-        yaxis: {range: [0, 1],
+        yaxis: {range: [-1, 1],
                 title: 'Scaled Sensors',
                 side: 'left'},
         yaxis2: {title: 'Failure Prediction',
@@ -107,61 +77,41 @@ function initPlot(){
     data[1].y = Array.from(initData[1].y);
     data[2].x = Array.from(initData[2].x);
     data[2].y = Array.from(initData[2].y);
-    data[3].x = Array.from(initData[3].x);
-    data[3].y = Array.from(initData[3].y);
-    data[4].x = Array.from(initData[3].x);
-    data[4].y = Array.from(initData[3].y);
+
     Plotly.newPlot('predGraph', data, layout);
 }
 
 var msgCounter = 0; // Another way of shifting. Not used in this code.
 
 function updatePlot(jsonData){
-    console.log("plot.js updatePlot()  " + jsonData);
+   // console.log("plot.js updatePlot()  " + jsonData);
     //console.log("msgCounter: " + msgCounter++);
     let max = 200;
     let jsonObj = JSON.parse(jsonData);  // json obj is in form:  ['timestamp', 'sensorVal']
-    //plot_dict = {
-    //        'timestamp': [row[0].timestamp],
-   //         'sensor0': [row[0].sensor25],
-   //         'sensor1': [row[0].sensor11],
-    //        'sensor2': [row[0].sensor36],
-    //        'sensor3': [row[0].sensor34],
-    //        'alarm': [0]
-     //   }
+ //           plot_dict = {
+//            'timestamp': one_row_df.index[0],
+//            'pc1': one_row_df['pc1'].values[0],
+//            'pc2': one_row_df['pc2'].values[0],
+//            'alarm': alarm_value
+//        }
 
     let timestamp = jsonObj.timestamp;
-    let sensor25 = jsonObj.sensor0;
-    let sensor11 = jsonObj.sensor1;
-    let sensor36 = jsonObj.sensor2;
-    let sensor34 = jsonObj.sensor3;
+    let pc1 = jsonObj.pc1;
+    let pc2 = jsonObj.pc2;
     let alarm =    jsonObj.alarm;
 
 
 
-    // Add new data point as well as calculated data for regression line start and end points as well as the
-    // y difference plot.  Note there are three traces.  The first two traces use the same layout named yaxis.
+    // Note there are three traces.  The first two traces use the same layout named yaxis.
+    // The first two traces plot the values of the two chosen pc's
     // The third trace uses the layout named yaxis2.  This naming convention follows that of plotly.js
+    // The third trace plots vertical red lines showing the predictions
     Plotly.extendTraces('predGraph', {
-        // NOTE:  The below 2 commented lines would be used if the dynamic line coloring were supported by plotly.js
-        //x: [[timestamp], [x1, x2], [diff_x1, diff_x2]],
-        //y: [[sensorValue], [y1,y2], [diff_y1, diff_y2]]
-       // x: [[timestamp], [x1, x2], [diff_x2]],
-       // y: [[sensorValue], [y1,y2], [diff_y2]]
-        x: [[timestamp], [timestamp], [timestamp], [timestamp] ],
-        y: [[sensor25], [sensor11], [sensor36], [sensor34]]
 
-    }, [0, 1, 2,3], max);  // The array denotes to plot all three traces(0 based).  Keep only last max data points
+        x: [[timestamp], [timestamp], [timestamp, timestamp] ],
+        y: [[pc1], [pc2], [0, alarm]]
 
+    }, [0, 1, 2], max);  // The array denotes to plot all three traces(0 based).  Keep only last max data points
 
-    // NOTE:  This code below was an attempt to dynamically change the color of the Percent Diff plot to red whenever
-    // the plot went beyond the acceptable range.  Plotly.js does not support such a feature.
-
-    //if(plot_color != null){
-    //    let updateStr = 'marker.color[' + row_counter + ']';
-        //Plotly.restyle('graph', editObj, [2]);
-    //    Plotly.react(graph, {[updateStr]: plot_color}, null, [2]);
-
-    //}
 
 }
