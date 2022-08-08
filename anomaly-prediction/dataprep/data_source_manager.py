2579c0a8848563a4818b6bc75b2d9b8fb698e037
guiderae <guiderae@yahoo.com>
2022-08-08 07:51:20 -0700

    Added documentation

--- anomaly-prediction/dataprep/data_source_manager.py
+++ anomaly-prediction/dataprep/data_source_manager.py
@@ -13,7 +13,7 @@ class DataSourceManager:
         :param file_name: Name of csv file as source of data
         :return: none
         ..notes:: This static method has no return.  Instead, it yields a row of data that has been read from
-        a data source.
+        a data source.  The row is yielded as a dictionary
         """
         with open(file_name, 'r') as read_obj:
             dict_reader = DictReader(read_obj)
