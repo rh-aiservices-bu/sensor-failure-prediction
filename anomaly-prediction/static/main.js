638a035fdfd4d0851cd2e58eb773bf368beb8e96
guiderae <guiderae@yahoo.com>
2022-08-09 15:15:13 -0700

    Put in missing semicolons

--- anomaly-prediction/static/main.js
+++ anomaly-prediction/static/main.js
@@ -1,12 +1,12 @@
 let displayDataDiv = document.getElementById("displayDataDiv");
-let dataPrepStartBtn = document.getElementById('startBtn')
+let dataPrepStartBtn = document.getElementById('startBtn');
 window.onload = async function(){
     console.log("window.onload");
     let url = '/initData'
     postInitData(url)
         .then(jsonData =>  displayJson(jsonData))
         .catch(error => console.error(error))
-}
+    };
 
 async function postInitData(url){
     return fetch(url,{
