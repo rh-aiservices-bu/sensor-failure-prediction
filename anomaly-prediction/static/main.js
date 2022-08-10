5c1b68bd0fd381236eb6c15619ff7c2086b23ab2
guiderae <guiderae@yahoo.com>
2022-08-10 07:16:55 -0700

    Moved call to onloadDataCheck() from function displayJson() to the window.onload event. Added function definitions onloadDataCheck() and doTwoFilesExist()

--- anomaly-prediction/static/main.js
+++ anomaly-prediction/static/main.js
@@ -6,7 +6,7 @@ window.onload = async function(){
     postInitData(url)
         .then(jsonData =>  displayJson(jsonData))
         .catch(error => console.error(error))
-    };
+    }; onloadDataCheck();
 
 async function postInitData(url){
     return fetch(url,{
@@ -20,6 +20,34 @@ function displayJson(jsonData){
    // Parse json, put values into html page elements
     displayDataDiv.innerHTML = jsonData.data;
     dataPrepStartBtn.disabled = false;
-    onloadDataCheck();
+    //onloadDataCheck();
+
+}
+// Determine if both files exist on server.  Return true if both exist, false otherwise
+// Return as a Promise a boolean that is true if both files exist, false if otherwise
+async function doTwoFilesExist(fileName1, fileName2){
+    // Do a fetch for each filename
+    let response1 = await fetch(fileName1, {method: 'HEAD'});
+    let response2 = await fetch(fileName2, {method: 'HEAD'});
+    let retValBool = response1.status == 200 && response2.status == 200;
+    return retValBool;
+
+
+}
+// When Data Prep tab is clicked, first determined if model has already been trained and scaler exists.
+// If both exist, enable Prediction Tab
+function onloadDataCheck(){
+    let fileName1 = 'static/trained_model/saved_model.pb';
+    let fileName2 = 'static/training_scaler.gz'
+    let dataPresent = doTwoFilesExist(fileName1, fileName2); //This is a Promise that was returned from the
+                                                             //async function, doTwoFilesExist()
+
+    dataPresent.then(function(result){  // The variable, dataPresent is a Promise,
+    if(result){
+        predTabBtn.disabled = false;
+    }else{
+        predTabBtn.disabled = true;  // This could be redundant since when page loads, the btn is disabled.
+    }
+    });
 
 }
