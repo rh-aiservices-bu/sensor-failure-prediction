9c6b2da74fe46788f8e840310220ed8d9691732e
guiderae <guiderae@yahoo.com>
2022-08-10 07:13:35 -0700

    Moved functions doTwoFilesExist() and onloadDataCheck to main.js

--- anomaly-prediction/static/btnControllers.js
+++ anomaly-prediction/static/btnControllers.js
@@ -30,34 +30,8 @@ startDataPrepBtn.addEventListener('click', function(){
     startDataPrepControlButtons(this);
 })
 
-// Determine if both files exist on server.  Return true if both exist, false otherwise
-// Return as a Promise a boolean that is true if both files exist, false if otherwise
-async function doTwoFilesExist(fileName1, fileName2){
-    // Do a fetch for each filename
-    let response1 = await fetch(fileName1, {method: 'HEAD'});
-    let response2 = await fetch(fileName2, {method: 'HEAD'});
-    let retValBool = response1.status == 200 && response2.status == 200;
-    return retValBool;
 
 
-}
-// When Data Prep tab is clicked, first determined if model has already been trained and scaler exists.
-// If both exist, enable Prediction Tab
-function onloadDataCheck(){
-    let fileName1 = 'static/trained_model/saved_model.pb';
-    let fileName2 = 'static/training_scaler.gz'
-    let dataPresent = doTwoFilesExist(fileName1, fileName2); //This is a Promise that was returned from the
-                                                             //async function, doTwoFilesExist()
-
-    dataPresent.then(function(result){  // The variable, dataPresent is a Promise,
-    if(result){
-        predTabBtn.disabled = false;
-    }else{
-        predTabBtn.disabled = true;  // This could be redundant since when page loads, the btn is disabled.
-    }
-    });
-
-}
 // When the Start Data Prep btn is clicked, Disable train tab, test tab, and predict tab
 function startDataPrepControlButtons(){
     trainTabBtn.disabled = true;
